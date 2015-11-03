b8d8e0fcb2e7b50f29bc2fc5bcb65df83e3c72a0
VinayakKkini <vinayakkini@Vinayaks-MacBook-Pro.local>
2015-11-03 08:57:00 +0530

    Initial project Commit

---
+++ SatSlide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SatSlide
+//
+//  Created by Vinayak Kini on 03/11/15.
+//  Copyright (c) 2015 Vinayak Kini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SatSlide/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SatSlide
+//
+//  Created by Vinayak Kini on 03/11/15.
+//  Copyright (c) 2015 Vinayak Kini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
